8bcdc385a730cca5ac6776f8ff636034a1e72909
Babatunde Alfred Olorundajo <tundealfred.office@gmail.com>
2024-12-21 02:17:57 +0000

    update contact page

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,11 +5,16 @@ export default {
     extend: {
       animation: {
         fadeIn: "fadeIn 0.5s ease-in-out",
+        "slide-up": "slideUp 0.6s ease-out",
       },
       keyframes: {
         FadeIn: {
           "0%": { opacity: 0 },
           "100%": { opacity: 1 },
+        },
+        slideUp: {
+          "0%": { transform: "translateY(20px)", opacity: 0 },
+          "100%": { transform: "translateY(0)", opacity: 1 },
         },
       },
     },
