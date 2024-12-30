5127494836a38b51f64457c1938add73756dddac
Babatunde Alfred Olorundajo <tundealfred.office@gmail.com>
2024-12-30 17:18:28 +0000

    daily update

--- server/server.js
+++ server/server.js
@@ -1,22 +1,22 @@
 "use strict";
 
-// this library lets us access our .env file
-require("dotenv").config();
-
 // express is a server library
 const express = require("express");
-
-// initalizes the express library
-const app = express();
 
 // library that determines who is allowed to speak to our server
 const cors = require("cors");
 
-// this settting says that everyone is allowed to speak to our server
-app.use(cors());
+// this library lets us access our .env file
+require("dotenv").config();
+
+// initalizes the express library
+const app = express();
 
 // we are getting the port variable from the .env file.
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
+
+// this settting says that everyone is allowed to speak to our server
+app.use(cors());
 
 // this is a route. if you turn the server on and go to http://localhost:3001/ (or whatever port you specified in your .env), you will see 'hello from the home route'
 app.get("/", (request, response) => {
